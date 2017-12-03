5403c2327f723c25687846d71536f39e2870aef1
clach04 <Chris.Clark@actian.com>
2017-12-02 21:30:35 -0800

    Document config contents

--- src/c/configuration.h
+++ src/c/configuration.h
@@ -5,15 +5,16 @@
 #define DEFAULT_TIME_ZONE +1
 #define NUM_SECRETS 2
 
-char otp_labels[NUM_SECRETS][17] = {
-	"gtest","fake",
+char otp_labels[NUM_SECRETS][17] = {  // labels for otp_keys[]
+	"gtest",
+    "fake",
 };
 
 unsigned char otp_keys[NUM_SECRETS][10] = {
-	{ 0x66, 0x6F, 0x6F, 0x6F, 0x6F, 0x6F, 0x6F, 0x6F },
+	{ 0x66, 0x6F, 0x6F, 0x6F, 0x6F, 0x6F, 0x6F, 0x6F },  // secret (in base32) "MZXW633PN5XW6===" == 'fooooooo' , See https://github.com/google/google-authenticator/issues/70
 	{ 0x7C, 0x94, 0x50, 0xEA, 0xA7, 0x2A, 0x08, 0x66, 0xA3, 0x47 },
 };
 
-int otp_sizes[NUM_SECRETS] = {8,10,};
+int otp_sizes[NUM_SECRETS] = {8,10,};  // number of bytes for otp_keys[] entries
 
 #endif
