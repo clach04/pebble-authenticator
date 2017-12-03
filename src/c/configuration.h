df1804ad091002d91fe8a1247b3dc5375bedf35b
clach04 <Chris.Clark@actian.com>
2017-12-02 23:01:04 -0800

    add base32 for 2nd example

--- src/c/configuration.h
+++ src/c/configuration.h
@@ -12,7 +12,7 @@ char otp_labels[NUM_SECRETS][17] = {  // labels for otp_keys[]
 
 unsigned char otp_keys[NUM_SECRETS][10] = {
 	{ 0x66, 0x6F, 0x6F, 0x6F, 0x6F, 0x6F, 0x6F, 0x6F },  // secret (in base32) "MZXW633PN5XW6===" == 'fooooooo' , See https://github.com/google/google-authenticator/issues/70
-	{ 0x7C, 0x94, 0x50, 0xEA, 0xA7, 0x2A, 0x08, 0x66, 0xA3, 0x47 },
+	{ 0x7C, 0x94, 0x50, 0xEA, 0xA7, 0x2A, 0x08, 0x66, 0xA3, 0x47 },  // secret (in base32) "PSKFB2VHFIEGNI2H"
 };
 
 int otp_sizes[NUM_SECRETS] = {8,10,};  // number of bytes for otp_keys[] entries
